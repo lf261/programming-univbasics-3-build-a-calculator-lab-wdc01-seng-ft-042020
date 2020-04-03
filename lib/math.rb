b815158a025ef7451e6b8fcb7a6cdad9c9d141e5
Luis Fernandez <luis.fernandez2093@gmail.com>
2020-04-03 21:43:03 +0000

    Automatically backed up by Learn

--- lib/math.rb
+++ lib/math.rb
@@ -4,22 +4,18 @@ end
 
 def subtraction(num1, num2)
   result = num1 - num2 
-  puts result 
 end
 
 def division(num1, num2)
   result = num2 / num1 
-  puts result
 end
 
 def multiplication(num1, num2)
   result = num1 * num2
-  puts result
 end
 
 def modulo(num1, num2)
   result = num2 % num1
-  puts result
 end
 
 def square_root(num)
